be6e48fc53871ae5f0fd3954b3da15a9c0b417a7
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2024-06-28 11:46:37 +0300

    check

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,6 @@ import { ChatModule } from './chat/chat.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { SocChatModule } from './soc-chat/soc-chat.module';
 import { StripeModule } from './stripe/stripe.module';
-import { SocChatService } from './soc-chat/soc-chat.service';
-import { SocChatGateway } from './soc-chat/soc-chat.gateway';
 
 @Module({
   imports: [
